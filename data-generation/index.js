a6d14dddd594b2e95682f4f321f59000fde3c32c
Jason Green <jasoniangreen@gmail.com>
2016-07-13 10:58:01 +0100

    changed

--- data-generation/index.js
+++ data-generation/index.js
@@ -1,9 +1,9 @@
 'use strict';
 
 var jsf = require('json-schema-faker');
-//var userSchema = require('./user-schema-1.json');
+var userSchema = require('./user-schema-1.json');
 //var userSchema = require('./user-schema-2.json');
-var userSchema = require('./user-schema-3.json');
+//var userSchema = require('./user-schema-3.json');
 
 var sample = jsf(userSchema);
 
